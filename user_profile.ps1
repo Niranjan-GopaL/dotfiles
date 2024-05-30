a565e111ad7dce08aee8ed10246619ff059a2168
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2024-05-31 01:03:05 +0530

    Added more themes .omp.json

--- user_profile.ps1
+++ user_profile.ps1
@@ -1,8 +1,12 @@
 Import-Module PSReadLine
 
 # Loading Theme and Shell :- All previes of themes are available in :- https://ohmyposh.dev/docs/themes
-#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\hul10.omp.json" | Invoke-Expression
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\huvix.omp.json" | Invoke-Expression
+ 
+  oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\kushal.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\kali.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\json.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\hul10.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\huvix.omp.json" | Invoke-Expression
 
 
 Function CD32 {Set-Location -Path D:\"Code Practise"}
